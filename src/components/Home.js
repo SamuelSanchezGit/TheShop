0a6f5dc101ddd59d9d17d533ba9ad271c4958adb
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-08-02 14:51:25 +0200

    fixHomeTitle

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center">
         <h2 className="text-4xl text-white mb-4 text-center">Bienvenue chez The Shop</h2>
-        <p className="text-xl text-white text-center">Le temple des supercars et des motos</p>
+        <p className="text-xl text-white text-center">Le temple de la passion auto et moto</p>
       </div>
     </div>
   );
